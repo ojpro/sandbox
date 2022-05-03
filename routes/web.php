387ef3d8d92af12c63b2b8c08a24b0563f9ec6ae
ojpro <oussamaeljabbari.mail@gmail.com>
2022-05-03 21:28:46 +0000

    Fix Bug 🐞: Redirect all sub-routes to vue app.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any?}', function () {
 	return view('app');
-});
+})->where(['any' => '.*']);
 
 Route::get('/dashboard', function () {
 	return view('dashboard');
